4a10009ec1e16733fd4a07b41c3d10040806db31
rokuro001 <114669748+rokuro001@users.noreply.github.com>
2023-07-06 18:06:18 +0800

    Add files via upload

--- ProjectAkhir/Project1.dpr
+++ ProjectAkhir/Project1.dpr
@@ -6,7 +6,8 @@ uses
   Unit2 in 'Unit2.pas' {DataModule2: TDataModule},
   Unit3 in 'Unit3.pas' {Form3},
   Unit4 in 'Unit4.pas' {Form4},
-  Unit5 in 'Unit5.pas' {Form5};
+  Unit5 in 'Unit5.pas' {Form5},
+  Unit6 in 'Unit6.pas' {Form6};
 
 {$R *.res}
 
@@ -17,5 +18,6 @@ begin
   Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TForm4, Form4);
   Application.CreateForm(TForm5, Form5);
+  Application.CreateForm(TForm6, Form6);
   Application.Run;
 end.
---
+++ ProjectAkhir/Unit6.pas
@@ -0,0 +1,28 @@
+unit Unit6;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, DB;
+
+type
+  TForm6 = class(TForm)
+    DataSource1: TDataSource;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form6: TForm6;
+
+implementation
+
+uses
+  Unit2;
+
+{$R *.dfm}
+
+end.
